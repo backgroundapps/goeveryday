ab454c4ddd674dfd78d8d4588551e3a3511c6edd
Jonatas Emidio <jonatasemidio@gmail.com>
2018-01-19 12:46:34 -0200

    Update variables-with-initializers.go

--- lessons/variables-with-initializers.go
+++ lessons/variables-with-initializers.go
@@ -1,1 +1,11 @@
 //https://tour.golang.org/basics/9
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+  var c, python, java = true, false, "no!"
+  fmt.Println(i, j, c, python, java)
+}
