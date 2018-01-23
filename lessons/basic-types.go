36211a4e10e654398f571768cd6ae3c335b0ad1c
Jonatas Emidio <jonatasemidio@gmail.com>
2018-01-23 10:29:04 -0200

    Update basic-types.go

--- lessons/basic-types.go
+++ lessons/basic-types.go
@@ -1,1 +1,19 @@
 //https://tour.golang.org/basics/11
+
+package main
+import (
+  "fmt"
+  "math/cmplx"
+)
+
+var(
+ ToBe bool = false
+ MaxInt uint64 = 1<<64 - 1
+ z complex128 = cmplx.Sqrt(-5 + 12i) 
+)
+
+func main() {
+  fmt.Printf("Type: %T and vlaue: %v\n", ToBe, ToBe)
+  fmt.Printf("Type: %T and vlaue: %v\n", MaxInt, MaxInt)
+  fmt.Printf("Type: %T and vlaue: %v\n", z, z)
+}
