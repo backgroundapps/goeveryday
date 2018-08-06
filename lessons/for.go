ec671dc7ec16d8998aab11d399af001fb73326c5
Jonatas Emidio <jonatasemidio@gmail.com>
2018-08-06 12:00:50 -0300

    Update for.go

--- lessons/for.go
+++ lessons/for.go
@@ -1,1 +1,14 @@
-//https://tour.golang.org/flowcontrol/1
+// https://tour.golang.org/flowcontrol/1
+
+package main
+
+import "fmt"
+
+func main() {
+  sum := 0
+  for i := 0; i < 10; i++ {
+    sum += i
+  }
+  fmt.Println(sum)
+
+}
